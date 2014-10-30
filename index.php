f1b57312c1ea4e59a94d0933af61c22f90a9ea58
pn024689 <pcp.nguyen@gmail.com>
2014-10-30 18:02:54 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+<link rel="stylesheet" type="text/css" href="loginForm.css">
+</head>
+
+<body>
+
+<form class="login">
+test
+
+
+</form>
+</body>
+
+</html>
