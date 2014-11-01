9cd0b3236c00022e047a5d46ccef13ca02622f1e
pn024689 <pcp.nguyen@gmail.com>
2014-11-01 12:40:54 +1100

    Updated
    Completed login forms and styling.
    Included transitions.

--- index.php
+++ index.php
@@ -6,14 +6,39 @@
 <link rel="stylesheet" type="text/css" href="loginForm.css">
 </head>
 
+
 <body>
 
-<form class="login">
-test
-hello world
-testing github
 
-</form>
+	<form action="login.php" class="login">
+	<h1>UTS Travel Funding
+		<span>Login with your UTS ID & Password</span>
+	</h1>
+
+	<label>
+		<span>Username</span>
+		<input type="text" name="Username" placeholder="Username">
+	</label>
+
+	<label>
+		<span>Password</span>
+		<input type="password" name="Password" placeholder="Password">
+	</label>
+
+
+	<label>
+		<span>&nbsp</span>
+		<input type="submit" value="Login" class="button">
+	</label>
+
+	<label>
+		<span>&nbsp</span>
+	<a href="https://email.itd.uts.edu.au/webapps/myaccount/passwordreset/" target="blank">Forgot Password?</a>
+	</label>
+	</form> 
+
+
+
 </body>
 
 </html>
