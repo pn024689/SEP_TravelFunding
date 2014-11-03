df8437f373fce111dabb51bfeddbcfc2c0fec07b
pn024689 <pcp.nguyen@gmail.com>
2014-11-03 17:15:21 +1100

    Main Page
    Added page after login

---
+++ main.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="content.css">
+	<title></title>
+
+
+
+
+</head>
+
+<body>	
+	<div id="header">
+		Logged in as *PHP ECHO NAME HERE* <a href="#">(Logout)</a>
+	</div>
+	<div id="contentArea">
+		<h1>UTS Travel Funding System</h1>
+
+		<div class="contentColumn">
+			<h2>My Details</h2>
+			<pre>
+			To edit your details, log into <a href="https://onestopadmin.uts.edu.au/eStudent/login.aspx?ReturnUrl=%2festudent%2fDefault.aspx" target="blank">UTS: myStudentAdmin</a>.
+
+			<em>UTS ID</em>
+			<em>Name</em>
+			<em>Date of Birth</em>
+			<em>E-mail Address</em>
+			<em>Phone Number</em>
+			<em>Address</em>
+			</pre>
+
+		</div>
+
+		<div class="contentColumn">
+			<h2>Applications</h2>
+			<pre>
+			To lodge an application, click on the "+" button.
+
+			</pre>
+		</div>
+
+
+
+
+
+
+
+
+
+
+
+	</div>
+
+
+
+
+
+
+
+</body>
+
+
+</html>
+
